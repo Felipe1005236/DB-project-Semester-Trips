7efdf8f4a5d484d06f75ce4c3d9b47174b3532c4
FelipeRibadeneira <fribadenei@constructor.university>
2024-10-17 20:39:37 +0200

    mysqli

--- PHP/connect_ser.php
+++ PHP/connect_ser.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Hello</h1>
     <?php
-        $connection = new msqli($host, $user, $password, $db);
+        $connection = new mysqli($host, $user, $password, $db);
 
         if($connection->connect_errno){
             echo "Failed to connect to db " . $mysqli->connect_error;
