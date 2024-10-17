eaf72700fd8d8575534f7b66e095e7da5fb13110
FelipeRibadeneira <fribadenei@constructor.university>
2024-10-17 20:35:04 +0200

    a new beginning

--- PHP/connect_ser.php
+++ PHP/connect_ser.php
@@ -15,5 +15,17 @@
             exit();
         }
         
+        $student_id = $_REQUEST['s_id'];
+        $student_destination = $_REQUEST['s_destination'];
+        $student_rating = $_REQUEST['s_rating'];
+        $e_id = $_REQUEST['e_id'];
+        $e_destination = $_REQUEST['e_destination'];
+        $e_rating = $_REQUEST['e_rating'];
+        $s_destination = $_REQUEST['s_destination'];
+        $s_location = $_REQUEST['s_location'];
+        $s_opening_time = $_REQUEST['opening_time'];
+        $e_destination = $_REQUEST['e_destination'];
+        $e_location = $_REQUEST['e_location'];
+        $e_time = $_REQUEST['e_time'];
         
     ?>
