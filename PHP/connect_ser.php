6db8a40daf089f58b9472ca686491a8ddae79520
FelipeRibadeneira <fribadenei@constructor.university>
2024-10-17 23:58:36 +0200

    Assignment 5

--- PHP/connect_ser.php
+++ PHP/connect_ser.php
@@ -4,7 +4,7 @@ $servername = "5.75.182.107";
 $username = "rfelipe";
 $password = "Diez.cinco5";
 $dbname = "rfelipe_db";
-
+ 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
