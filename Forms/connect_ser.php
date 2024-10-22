9023e098d8c89f6d642257e3e8b74a253393ea98
FelipeRibadeneira <fribadenei@constructor.university>
2024-10-22 16:28:16 +0200

    Fixed syntax error in connect.php

--- Forms/connect_ser.php
+++ Forms/connect_ser.php
@@ -15,10 +15,10 @@
 
     include 'requirements.php'
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $connectionection = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -33,8 +33,8 @@
 
                 case 'Activities':
                     
-                    $fee = $conn->real_escape_string($_POST['fee']);
-                    $act_id = $conn->real_escape_string($_POST['att_id']);
+                    $fee = $connection->real_escape_string($_POST['fee']);
+                    $act_id = $connection->real_escape_string($_POST['att_id']);
 
                     $sql = "INSERT INTO Activity (activity_id, entry_fee) VALUES ('$att_id', '$fee')";
 
@@ -42,8 +42,8 @@
 
                 case 'Attractions':
 
-                    $fee = $conn->real_escape_string($_POST['fee']);
-                    $att_id = $conn->real_escape_string($_POST['att_id']);
+                    $fee = $connection->real_escape_string($_POST['fee']);
+                    $att_id = $connection->real_escape_string($_POST['att_id']);
 
                     $sql = "INSERT INTO Attraction (att_id, fee) VALUES ('$att_id','$fee')";
 
@@ -51,11 +51,11 @@
 
                 case 'Destination':
                     
-                    $dest_id = $conn->real_escape_string($_POST['dest_id']);
-                    $dest_name = $conn->real_escape_string($_POST['name']);
-                    $location = $conn->real_escape_string($_POST['location']);
-                    $opening = $conn->real_escape_string($_POST['opening_time']);
-                    $closing = $conn->real_escape_string($_POST['closing_time']);
+                    $dest_id = $connection->real_escape_string($_POST['dest_id']);
+                    $dest_name = $connection->real_escape_string($_POST['name']);
+                    $location = $connection->real_escape_string($_POST['location']);
+                    $opening = $connection->real_escape_string($_POST['opening_time']);
+                    $closing = $connection->real_escape_string($_POST['closing_time']);
 
                     $sql = "INSERT INTO Destination (dest_id, name, location, opening_time, closing_time) VALUES ('$dest_id', '$dest_name', '$location', '$opening', '$closing')";
 
@@ -63,9 +63,9 @@
 
                 case 'Editor':
                     
-                    $editor_id = $conn->real_escape_string($_POST['editor_id']);
-                    $type = $conn->real_escape_string($_POST['type']);
-                    $username = $conn->real_escape_string($_POST['username']);
+                    $editor_id = $connection->real_escape_string($_POST['editor_id']);
+                    $type = $connection->real_escape_string($_POST['type']);
+                    $username = $connection->real_escape_string($_POST['username']);
 
                     $sql = "INSERT INTO Editor (editor_id, type, username) VALUES ('$editor_id', '$type', '$username')";
 
@@ -73,8 +73,8 @@
 
                 case 'Food':
                     
-                    $food_id = $conn->real_escape_string($_POST['food_id']);
-                    $cuisine_type = $conn->real_escape_string($_POST['cuisine_type']);
+                    $food_id = $connection->real_escape_string($_POST['food_id']);
+                    $cuisine_type = $connection->real_escape_string($_POST['cuisine_type']);
 
                     $sql = "INSERT INTO Food_place (food_id, cuisine_type) VALUES ('$food_id', '$cuisine_type')";
 
@@ -82,10 +82,10 @@
 
                 case 'Review':
                     
-                    $review_id = $conn->real_escape_string($_POST['review_id']);
-                    $destination_id = $conn->real_escape_string($_POST['dest_id']);
-                    $rating = $conn->real_escape_string($_POST['rating']);
-                    $comment = $conn->real_escape_string($_POST['comment']);
+                    $review_id = $connection->real_escape_string($_POST['review_id']);
+                    $destination_id = $connection->real_escape_string($_POST['dest_id']);
+                    $rating = $connection->real_escape_string($_POST['rating']);
+                    $comment = $connection->real_escape_string($_POST['comment']);
 
                     $sql = "INSERT INTO Reviews (review_id, dest_id, rating, comment) VALUES ('$review_id', '$dest_id', '$rating', '$comment')";
 
@@ -93,10 +93,10 @@
 
                 case 'Student':
 
-                    $student_id = $conn->real_escape_string($_POST['student_id']);
-                    $school = $conn->real_escape_string($_POST['school']);
-                    $username = $conn->real_escape_string($_POST['username']);
-                    $age = $conn->real_escape_string($_POST['age']);
+                    $student_id = $connection->real_escape_string($_POST['student_id']);
+                    $school = $connection->real_escape_string($_POST['school']);
+                    $username = $connection->real_escape_string($_POST['username']);
+                    $age = $connection->real_escape_string($_POST['age']);
 
                     $sql = "INSERT INTO Student (student_id, school, username, age) VALUES ('$student_id', '$school', '$username', '$age')";
 
@@ -104,9 +104,9 @@
 
                 case 'User':
                     
-                    $user_id = $conn->real_escape_string($_POST['user_id']);
-                    $user_name = $conn->real_escape_string($_POST['name']);
-                    $email = $conn->real_escape_string($_POST['email']);
+                    $user_id = $connection->real_escape_string($_POST['user_id']);
+                    $user_name = $connection->real_escape_string($_POST['name']);
+                    $email = $connection->real_escape_string($_POST['email']);
 
                     $sql = "INSERT INTO User (user_id, name, email,) VALUES ('$user_id', '$user_name', '$email')"
 
@@ -117,11 +117,11 @@
                     exit;
             }
 
-            if($conn->query($sql) === TRUE){
+            if($connection->query($sql) === TRUE){
                 echo "Values Inserted correctly";
             } 
             else {
-                echo "Error: ", $sql . "<br>" . $conn->error;
+                echo "Error: ", $sql . "<br>" . $connection->error;
             }
         }
         else {
@@ -132,6 +132,6 @@
         echo "Invalid request";
     }
 
-    $conn->close();
+    $connection->close();
     ?>
 <div>
