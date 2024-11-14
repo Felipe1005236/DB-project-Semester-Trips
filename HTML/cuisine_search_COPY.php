7775172857b8553cbd1db5d0d02f24fd7d0df65b
Bisera Kjurchinska <bkjurchins@constructor.university>
2024-11-14 17:55:04 +0100

    trying my best

--- HTML/cuisine_search_COPY.php
+++ HTML/cuisine_search_COPY.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html>
-
+global$dbname; global$password; global$username; global$servername; <!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>
-        Railway Getaway
-    </title>
-    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../CSS/search_page.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/smoothness/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.js"></script>
 </head>
-
 <body>
-    <div class="img_container">
+
+<div class="img_container">
         <img src="../images/window-transformed.png" alt="background" id="window">
         <!--user inputs a destination, gets attractions there and entry fee of those attractions-->
 
@@ -25,8 +19,10 @@
             <input type="hidden" name="input_type" value="cuisine">
             <form action="connect_ser.php" method="post">
             <div class="aligning_questions">
-                <label for="autocomplete" class="question"> Cuisine type:</label>
-                <input itemid="autocomplete" type="text" id="cuisine_type" name="cuisine_type" placeholder="e.g. Chinese, German">
+            <label for="autocomplete">Select a Location</label>
+            <input id ="autocomplete">
+                <!-- <label class="question"> Cuisine type:</label>
+                <input type="text" id="cuisine_type" name="cuisine_type" placeholder="e.g. Chinese, German"> -->
             </div>
             <br>
             <div class="button-container">
@@ -37,10 +33,11 @@
         <div>
             <h3><a class="link" href="../HTML/search_page.html">Back to Search Page</a></h3>
         </div>
+</div>
+<!-- <label for="autocomplete">Select a Location</label>
+<input id ="autocomplete"> -->
 
-    </div>
-
-    <?php
+<?php
 
 include "../Forms/requirements.php";
 
@@ -69,6 +66,5 @@ $real_result = json_encode($tags);
 echo "<script>$(\"#autocomplete\").autocomplete({source: $real_result});</script>";
 
 ?>
-    
 </body>
 </html>
