a1067fd23a117b42e3c299134c48b2dc222df94c
Madina Mazhenova <mmdina@MacBook-Air-Madina-2.local>
2024-11-14 15:26:39 +0100

    mememe

--- Forms/requirements.php
+++ Forms/requirements.php
@@ -1,8 +1,8 @@
 <?php
-    //$servername = "mysql";
-    $servername = "5.75.182.107";
+    $servername = "mysql";
+    // $servername = "5.75.182.107";
     $username = "rfelipe";
-    //$password = "cAAnKz";
-    $password = "Diez.cinco5";
+    $password = "cAAnKz";
+    // $password = "Diez.cinco5";
     $dbname = "rfelipe_db";
 ?>
