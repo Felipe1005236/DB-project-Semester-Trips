205509bf9fb39cea7921aed908a40b948efb4b3e
FelipeRibadeneira <fribadenei@constructor.university>
2025-01-05 01:25:37 +0100

    the final commit

--- Search_Files/search_connect.php
+++ Search_Files/search_connect.php
@@ -16,7 +16,7 @@
     <?php
         include "../Forms/requirements.php";
 
-        $connection = new mysqli($servername, $username, $password, $dbname)
+        $connection = new mysqli($servername, $username, $password, $dbname);
 
         if ($connection->connect_errno) {
             echo "Failed to connect to database. " . $mysqli->connect_error;
